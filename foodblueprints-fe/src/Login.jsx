1d7d542468ff216a6c25028b919014a694a2aad9
eliJI <elijahingr@gmail.com>
2023-04-07 16:53:16 -0400

    feat: added registration screen and fixed

--- foodblueprints-fe/src/Login.jsx
+++ foodblueprints-fe/src/Login.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import "./login.css";
 import axios from 'axios';
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useSignIn } from "react-auth-kit";
 
 function Login(props){
 
     const [u_pass,setPass] = useState();
-    const [u_email, setEmail] = useState();
-
+    const [u_email, setEmail] = useState();    
+    const navigate = useNavigate();
     const signIn = useSignIn();
 
     const handleSubmit = (e) => {
-        let values = {u_email, u_pass}
         e.preventDefault();
         axios.post("http://127.0.0.1:8080/api/v1/auth/authenticate", {
             "email": u_email,
@@ -21,9 +20,16 @@ function Login(props){
         .then(function (response) {
             
             console.log(response)
-            //signIn({
-                //response.
-            //});
+            console.log(response.data.access_token);
+            signIn({
+                token: response.data.access_token,
+                expiresIn:3600,
+                tokenType: "Bearer",
+                authState: { email: u_email},
+            });
+
+            navigate("/home")
+            
         })
         .catch(function(error) {
             console.log(error)
@@ -32,16 +38,18 @@ function Login(props){
     }
     return (
         <div className="Login">
-            <h2>Login</h2>
-            <Link to = "/home"> home </Link>
-            <form onSubmit = {handleSubmit} className="login-form">
-              
-                <input type = "text" onChange = {(e) => setEmail(e.target.value)}id = "user"  placeholder="enter username.."/>
-             
-                <input type = "text" id = "password" onChange = {(e) => setPass(e.target.value)} placeholder="enter password..."/>
+            <h1>FoodBlueprints</h1>
+            <h2>Welcome, please log in or sign up.</h2>
+            <div className="login-form-container">
+                <form onSubmit = {handleSubmit} className="login-form">
+                    <input type = "text" onChange = {(e) => setEmail(e.target.value)}id = "user"  placeholder="enter username.."/>
+                    <input type = "text" id = "password" onChange = {(e) => setPass(e.target.value)} placeholder="enter password..."/>
 
-                <input type = "submit" value = "Submit"/>
-            </form>
+                    <input type = "submit" value = "Submit"/>
+                </form>
+                <Link to = "/register">Don't have an account? Sign up</Link>
+            </div>
+           
         </div>
     );
 }
