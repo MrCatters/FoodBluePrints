b44a54e4de99e0de531ef8c13a3bbc3fb26dc558
MrCatters <73203569+MrCatters@users.noreply.github.com>
2023-04-07 14:14:08 -0400

    fixed: added CORS for port 3000

--- spring_backend/src/main/java/com/recipe/recipesite/controller/auth/AuthenticationController.java
+++ spring_backend/src/main/java/com/recipe/recipesite/controller/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.recipe.recipesite.controller.auth;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
