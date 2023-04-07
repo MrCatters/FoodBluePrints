d2adaad5ae053d48adc64de31dddfd4192798516
MrCatters <73203569+MrCatters@users.noreply.github.com>
2023-04-07 17:09:57 -0400

    added: recipe model

---
+++ spring_backend/src/main/java/com/recipe/recipesite/model/recipe/Recipe.java
@@ -0,0 +1,47 @@
+package com.recipe.recipesite.model.recipe;
+
+import java.sql.Time;
+
+
+import com.recipe.recipesite.model.users.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "recipes")
+public class Recipe {
+    
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Lob
+    private byte[] image;
+
+    private String ingredients;
+
+    private Time timeAdded;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ spring_backend/src/main/java/com/recipe/recipesite/model/recipe/RecipeRepository.java
@@ -0,0 +1,10 @@
+package com.recipe.recipesite.model.recipe;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
+    Optional<Recipe> findByName(String name);
+}
