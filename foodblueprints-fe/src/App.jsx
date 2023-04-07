8b8f9ad1504015e2e159fa401596a2e330f1d943
eliJI <elijahingr@gmail.com>
2023-04-07 14:05:21 -0400

    added proxy to package.json

--- foodblueprints-fe/src/App.jsx
+++ foodblueprints-fe/src/App.jsx
@@ -7,18 +7,14 @@ import ProtectedRoutes from "./ProtectedRoutes";
 import { Route, Routes } from "react-router-dom"
 
 function App() {
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
-  const [token,setToken] = useState();
   
   return (
     <Routes>
-      <Route path = "/" element = {<Login />} />
-      <Route element = {<ProtectedRoutes />}>
+      
+      <Route path = "/" element = {<Home />} />
+      <Route path = "/login" element = {<Login />} /> 
 
-        <Route path = "/home" element = {<Home />} /> 
-
-      </Route>
+     
     </Routes> 
   );
 }
--- foodblueprints-fe/src/Login.jsx
+++ foodblueprints-fe/src/Login.jsx
@@ -2,21 +2,48 @@ import React, { useState } from "react";
 import "./login.css";
 import axios from 'axios';
 import { Link } from "react-router-dom"
+import { useSignIn } from "react-auth-kit";
 
 function Login(props){
+
+    const [u_pass,setPass] = useState();
+    const [u_email, setEmail] = useState();
+
+    const signIn = useSignIn();
+
+    const handleSubmit = (e) => {
+        let values = {u_email, u_pass}
+        e.preventDefault();
+        axios.post("http://127.0.0.1:8080/api/v1/auth/authenticate", {
+            "email": u_email,
+            "password": u_pass,
+        })
+        .then(function (response) {
+            
+            console.log(response)
+            //signIn({
+                //response.
+            //});
+        })
+        .catch(function(error) {
+            console.log(error)
+        });
+
+    }
     return (
         <div className="Login">
             <h2>Login</h2>
             <Link to = "/home"> home </Link>
-            <form className="login-form">
-                <label for ="user">Username</label>
-                <input type = "text" id = "user"  placeholder="enter username.."/>
-                <label for = "password">Password</label>
-                <input type = "text" id = "password" placeholder="enter password..."/>
-                <button>Log in</button>
+            <form onSubmit = {handleSubmit} className="login-form">
+              
+                <input type = "text" onChange = {(e) => setEmail(e.target.value)}id = "user"  placeholder="enter username.."/>
+             
+                <input type = "text" id = "password" onChange = {(e) => setPass(e.target.value)} placeholder="enter password..."/>
+
+                <input type = "submit" value = "Submit"/>
             </form>
         </div>
     );
 }
 
-export default Login
+export default Login;
