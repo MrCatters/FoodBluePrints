1baff8d4a3e2e2b13389bbf923c0696283071b46
MrCatters <73203569+MrCatters@users.noreply.github.com>
2023-04-07 18:14:03 -0400

    fixed: missing commits

--- spring_backend/src/main/java/com/recipe/recipesite/model/recipe/Recipe.java
+++ spring_backend/src/main/java/com/recipe/recipesite/model/recipe/Recipe.java
@@ -7,12 +7,10 @@ import com.recipe.recipesite.model.users.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
