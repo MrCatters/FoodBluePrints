ebdf71aa02a913d7bc012af7dffc9355f8be4144
eliJI <elijahingr@gmail.com>
2023-04-06 21:03:29 -0400

    fix: added css styling and login form

--- foodblueprints-fe/src/Login.jsx
+++ foodblueprints-fe/src/Login.jsx
@@ -5,9 +5,16 @@ import { Link } from "react-router-dom"
 
 function Login(){
     return (
-        <div className="Return">
+        <div className="Login">
             <h2>Login</h2>
             <Link to = "/home"> home </Link>
+            <form className="login-form">
+                <label for ="user">Username</label>
+                <input type = "text" id = "user"  placeholder="enter username.."/>
+                <label for = "password">Password</label>
+                <input type = "text" id = "password" placeholder="enter password..."/>
+                <button>Log in</button>
+            </form>
         </div>
     );
 }
