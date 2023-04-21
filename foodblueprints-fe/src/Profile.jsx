66ff536fcd9e9d2e69a7dd05eae70febc6ac9609
eliJI <elijahingr@gmail.com>
2023-04-21 17:41:50 -0400

    fixed add apopup

---
+++ foodblueprints-fe/src/Profile.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import "./profile.css"
+
+function Profile() {
+    return (
+        
+        <div className="profile">
+            <Header />
+            <div className="container">
+                <h1>Welcome back, Joe</h1>   
+            </div>
+           
+        </div>
+    );
+}
+
+export default Profile
