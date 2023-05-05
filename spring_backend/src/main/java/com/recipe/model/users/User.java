4a021a4cce2317cec6d2d79f0733268e67ff6363
MrCatters <73203569+MrCatters@users.noreply.github.com>
2023-05-05 12:42:07 -0400

    removed: unused imports

--- spring_backend/src/main/java/com/recipe/model/users/User.java
+++ spring_backend/src/main/java/com/recipe/model/users/User.java
@@ -8,7 +8,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import lombok.AllArgsConstructor;
